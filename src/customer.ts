cb0f966a739deef881b768bf490b6a9f57d56858
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-08-19 16:21:07 -0300

    feat: create a Customer class

---
+++ src/customer.ts
@@ -0,0 +1,40 @@
+class Customer {
+  _id: string;
+  _name: string;
+  _address: string;
+  _active: boolean;
+
+  constructor(id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+    this._active = true;
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
+
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+}
