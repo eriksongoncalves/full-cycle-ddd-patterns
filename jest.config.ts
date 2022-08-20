267ad558b7d37bdce41567cf8b1c45b0c867bdd2
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-08-19 21:23:49 -0300

    choreç config jest

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    "^.+.(t|j)sx?$": ["@swc/jest"],
+  },
+  clearMocks: true,
+  coverageProvider: "v8",
+};
