8ac9b783169713f4534f24994d3f91a5643d504b
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-08-21 16:11:22 -0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address('Rua dois', 2, '12345-000', 'São Paulo');
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem('1', 'Item 1', 10);
-const item2 = new OrderItem('1', 'Item 2', 15);
+const item1 = new OrderItem('1', 'Item 1', 10, '1', 1);
+const item2 = new OrderItem('1', 'Item 2', 15, '2', 1);
 
-const order = new Order('1', customer._id, [item1, item2]);
+const order = new Order('1', '123', [item1, item2]);
