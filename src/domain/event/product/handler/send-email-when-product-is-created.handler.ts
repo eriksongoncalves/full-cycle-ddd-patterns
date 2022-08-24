975437882c22824bc83afa787792619e929b0958
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-08-23 21:45:47 -0300

    feat: register event

---
+++ src/domain/event/product/handler/send-email-when-product-is-created.handler.ts
@@ -0,0 +1,11 @@
+import EventHandlerInterface from '../../@shared/event-handler.interface';
+import eventInterface from '../../@shared/event.interface';
+import ProductCreatedEvent from '../product-created.event';
+
+export default class SendEmailWhenProductIsCreatedHandler
+  implements EventHandlerInterface<ProductCreatedEvent>
+{
+  handle(event: eventInterface): void {
+    console.log(`Send email to ${event.eventData.email}`);
+  }
+}
