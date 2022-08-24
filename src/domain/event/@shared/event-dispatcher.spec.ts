7fb4f74d675cb9043751921c2481d57b4587b71b
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-08-23 21:56:46 -0300

    feat: unregisterAll event

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -37,4 +37,21 @@ describe('Domain events tests', () => {
       0
     );
   });
+
+  it('should unregisterAll an event handler', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]
+    ).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent']
+    ).toBeUndefined();
+  });
 });
--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -37,6 +37,6 @@ export default class EventDispatcher implements EventDispatcherInterface {
   }
 
   unregisterAll(): void {
-    throw new Error('Method not implemented.');
+    this.eventHandlers = {};
   }
 }
